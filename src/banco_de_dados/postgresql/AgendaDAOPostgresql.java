5559ae251b24cdacd52739c4bc11c6fc5c254d05
Marcelo Torres <marcelo-torres>
2018-11-17 11:47:30 -0200

    Implementacao dos metodos de busca de varias agendas

--- src/banco_de_dados/postgresql/AgendaDAOPostgresql.java
+++ src/banco_de_dados/postgresql/AgendaDAOPostgresql.java
@@ -40,8 +40,8 @@ public class AgendaDAOPostgresql implements AgendaDAO{
         try(PreparedStatement statement = conexao.prepareStatement(sql)) {
             statement.setInt(1, id);
             statement.setString(2, diaDaSemana.toString());
-            statement.setString(3, horarioDeInicio.format(Agenda.padraoHorario));
-            statement.setString(4, horarioDoFim.format(Agenda.padraoHorario));
+            statement.setString(3, horarioDeInicio.format(Agenda.PADRAO_DE_HORARIO));
+            statement.setString(4, horarioDoFim.format(Agenda.PADRAO_DE_HORARIO));
             statement.setInt(5, donoDaAgenda.getCRM());
 
             statement.execute();
@@ -65,8 +65,8 @@ public class AgendaDAOPostgresql implements AgendaDAO{
         
         try(PreparedStatement statement = conexao.prepareStatement(sql)) {
             statement.setString(1, agenda.getDiaDaSemana().toString());
-            statement.setString(2, agenda.getHorarioDoInicio().format(Agenda.padraoHorario));
-            statement.setString(3, agenda.getHorarioDoFim().format(Agenda.padraoHorario));
+            statement.setString(2, agenda.getHorarioDoInicio().format(Agenda.PADRAO_DE_HORARIO));
+            statement.setString(3, agenda.getHorarioDoFim().format(Agenda.PADRAO_DE_HORARIO));
             statement.setInt(4, agenda.getDonoDaAgenda().getCRM());
             statement.setInt(5, agenda.getID());
             
@@ -100,6 +100,7 @@ public class AgendaDAOPostgresql implements AgendaDAO{
 
     @Override
     public Agenda buscar(int id) throws Exception {
+        
         Connection conexao = Conector.obterConexao();
         
         Agenda agenda = null;
@@ -129,12 +130,101 @@ public class AgendaDAOPostgresql implements AgendaDAO{
 
     @Override
     public LinkedList<Agenda> buscarPeloDia(DiaDaSemana diaDaSemana) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Connection conexao = Conector.obterConexao();
+        
+        LinkedList<Agenda> agendasEncontradas = new LinkedList();
+        
+        String sql = "SELECT * FROM " + NOME_COMPLETO + " WHERE dia_semana "
+                + "LIKE '%" + diaDaSemana + "%'";
+        try(ResultSet resultSet = conexao.createStatement().executeQuery(sql)) {
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id_agenda");
+                String diaDaSemanaEncontrado = resultSet.getString("dia_semana");
+                String horarioDoInicio = resultSet.getString("horario_inicio");
+                String horarioDoFim = resultSet.getString("horario_fim");
+                int crm = resultSet.getInt("fk_medico_crm");
+
+                Medico medico = new MedicoDAOPostgresql().buscarPeloCrm(crm);
+                
+                Agenda agenda = new Agenda(id, DiaDaSemana.obterValor(diaDaSemanaEncontrado),
+                        LocalTime.parse(horarioDoInicio), LocalTime.parse(horarioDoFim),
+                        medico);
+                
+                agendasEncontradas.add(agenda);
+            }
+        } catch(SQLException sqle) {
+            throw new Exception("Nao foi possivel encontrar esta agenda no banco de dados: " + sqle.getMessage());
+        } finally {
+            conexao.close();
+            return agendasEncontradas;
+        }
     }
 
     @Override
     public LinkedList<Agenda> buscarPeloCrm(int crm) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+        Connection conexao = Conector.obterConexao();
+        
+        LinkedList<Agenda> agendasEncontradas = new LinkedList();
+        
+        String sql = "SELECT * FROM " + NOME_COMPLETO + 
+                " WHERE fk_medico_crm = " + crm ;
+        try(ResultSet resultSet = conexao.createStatement().executeQuery(sql)) {
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id_agenda");
+                String diaDaSemana = resultSet.getString("dia_semana");
+                String horarioDoInicio = resultSet.getString("horario_inicio");
+                String horarioDoFim = resultSet.getString("horario_fim");
+                int crmEncontrado = resultSet.getInt("fk_medico_crm");
+
+                Medico medico = new MedicoDAOPostgresql().buscarPeloCrm(crmEncontrado);
+                
+                Agenda agenda = new Agenda(id, DiaDaSemana.obterValor(diaDaSemana),
+                        LocalTime.parse(horarioDoInicio), LocalTime.parse(horarioDoFim),
+                        medico);
+                
+                agendasEncontradas.add(agenda);
+            }
+        } catch(SQLException sqle) {
+            throw new Exception("Nao foi possivel encontrar esta agenda no banco de dados: " + sqle.getMessage());
+        } finally {
+            conexao.close();
+            return agendasEncontradas;
+        }
     }
     
+    @Override
+    public LinkedList<Agenda> buscarPeloCrmEPeloDia(int crm, DiaDaSemana diaDaSemana) throws Exception {
+    
+        Connection conexao = Conector.obterConexao();
+        
+        LinkedList<Agenda> agendasEncontradas = new LinkedList();
+        
+        String sql = "SELECT * FROM " + NOME_COMPLETO + 
+                " WHERE fk_medico_crm = " + crm + 
+                " AND dia_semana LIKE '%" + diaDaSemana + "%'";
+        try(ResultSet resultSet = conexao.createStatement().executeQuery(sql)) {
+            while(resultSet.next()) {
+                int id = resultSet.getInt("id_agenda");
+                String diaDaSemanaEncontrado = resultSet.getString("dia_semana");
+                String horarioDoInicio = resultSet.getString("horario_inicio");
+                String horarioDoFim = resultSet.getString("horario_fim");
+                int crmEncontrado = resultSet.getInt("fk_medico_crm");
+
+                Medico medico = new MedicoDAOPostgresql().buscarPeloCrm(crmEncontrado);
+                
+                Agenda agenda = new Agenda(id, DiaDaSemana.obterValor(diaDaSemanaEncontrado),
+                        LocalTime.parse(horarioDoInicio), LocalTime.parse(horarioDoFim),
+                        medico);
+                
+                agendasEncontradas.add(agenda);
+            }
+        } catch(SQLException sqle) {
+            throw new Exception("Nao foi possivel encontrar esta agenda no banco de dados: " + sqle.getMessage());
+        } finally {
+            conexao.close();
+            return agendasEncontradas;
+        }
+    }
 }
