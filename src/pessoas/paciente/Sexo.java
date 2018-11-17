2d0df0559f9202bb8f0e65cd3340cf96a869786e
Marcelo Torres <marcelo-torres>
2018-11-16 22:28:44 -0200

    Adicao de funcionalidade de busca de constante pela string

--- src/pessoas/paciente/Sexo.java
+++ src/pessoas/paciente/Sexo.java
@@ -1,22 +1,38 @@
 package pessoas.paciente;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author marcelo
- */
 public enum Sexo {
-    FEMININO(1), MASCULINO(2);
+    
+    FEMININO(1) {
+                
+        @Override
+        public String toString() {
+            return "feminino";
+        }
+    },
+    
+    MASCULINO(2) {
+        @Override
+        public String toString() {
+            return "masculino";
+        }
+    };
+    
+    
+    public static Sexo obterValor(String sexo) {
+    
+        if(sexo.equals(FEMININO.toString())) {
+            return FEMININO;
+        } else if(sexo.equals(MASCULINO.toString())) {
+            return MASCULINO;
+        }
+        
+        return null;
+    }
+    
     
     public int codigoDoSexo;
     
     private Sexo(int codigoDoSexo) {
         this.codigoDoSexo = codigoDoSexo;
     }
-    
 }
