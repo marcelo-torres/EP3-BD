0d7388794d1d66880ca99cfd2a04eb5ed5a76172
Marcelo Torres <marcelo-torres>
2018-11-19 17:56:09 -0200

    Adicao de novos metodos abstratos

--- src/banco_de_dados/dao/ConsultaDiagnosticoDAO.java
+++ src/banco_de_dados/dao/ConsultaDiagnosticoDAO.java
@@ -1,6 +1,7 @@
 package banco_de_dados.dao;
 
 import dados_da_clinica.Consulta;
+import dados_da_clinica.Doenca;
 import dados_da_clinica.Especialidade;
 import dados_da_clinica.FormaDePagamento;
 import java.time.LocalDate;
@@ -24,13 +25,17 @@ public interface ConsultaDiagnosticoDAO {
     
     public LinkedList<Consulta> buscarPelaData(LocalDate data) throws Exception;
     
+    public LinkedList<Consulta> buscarPorPeriodo(LocalDate dataInicial, LocalDate dataFinal) throws Exception;
+    
     public LinkedList<Consulta> buscarPeloPagou(Boolean pagou) throws Exception;
     
     public LinkedList<Consulta> buscarPelaFormaDePagamento(FormaDePagamento formaDePagamento) throws Exception;
     
-    public LinkedList<Consulta> buscarPelaEspecialidade(Especialidade especialidade) throws Exception;
+    public LinkedList<Consulta> buscarPelaEspecialidade(Integer codigoEspecialidade) throws Exception;
     
-    public LinkedList<Consulta> buscarPeloPaciente(Paciente paciente) throws Exception;
+    public LinkedList<Consulta> buscarPeloPaciente(Integer codigoPaciente) throws Exception;
     
-    public LinkedList<Consulta> buscarPeloMedico(Medico medico) throws Exception;
+    public LinkedList<Consulta> buscarPeloMedico(Integer crmMedico) throws Exception;
+    
+    public LinkedList<Consulta> buscarPelaDoenca(Integer idDoenca) throws Exception;
 }
